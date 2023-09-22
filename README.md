7dcdf137bac4f2c7a48cee29255b50924f2df105
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2023-09-22 01:12:56 +0100

    Add banner.txt

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Container API
 
 ## Pre-requisites
-- (JDK 20)[https://adoptium.net/en-GB/temurin/releases/]
-- (Docker)[https://docs.docker.com/get-docker/]
+- [JDK 20](https://adoptium.net/en-GB/temurin/releases/)
+- [Docker](https://docs.docker.com/get-docker/)
--- src/main/kotlin/com/pie/container/api/Application.kt
+++ src/main/kotlin/com/pie/container/api/Application.kt
@@ -1,13 +1,10 @@
 package com.pie.container.api
 
-import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class Application
 fun main(args: Array<String>) {
-    runApplication<Application>(*args) {
-        setBannerMode(Banner.Mode.OFF)
-    }
+    runApplication<Application>(*args)
 }
