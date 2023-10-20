1d2cd75c17c2da8cfe87810fc993dcca7dd3f5bc
marvincudjoe <18750678+marvincudjoe@users.noreply.github.com>
2023-10-20 15:51:40 +0100

    chore: Add dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.3"
     id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("org.jetbrains.dokka") version "1.9.10"
     kotlin("plugin.spring") version "1.9.10"
     kotlin("plugin.jpa") version "1.9.10"
 }
